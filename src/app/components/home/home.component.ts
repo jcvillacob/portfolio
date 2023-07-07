1c94bf5e822b8fa5e47e7b26ebe743ea7d96e3e0
Juan Camilo Villacob <jucaviza6@gmail.com>
2023-07-07 17:13:03 -0500

    home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  init: boolean = false;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.init = true;
+    }, 500);
+  }
 
 }
